4bab1d0759c7ede77d1a29a2027357b9f50b6e29
Renz Russell T. Wacan <rrwacan@gmail.com>
2023-11-24 05:24:49 +0800

    Increment 2

---
+++ app/src/main/java/com/example/prodoreviewer/Timeo_LogIn.java
@@ -0,0 +1,36 @@
+package com.example.prodoreviewer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Timeo_LogIn extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+    Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeo_log_in);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
+                    Toast.makeText(Timeo_LogIn.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(Timeo_LogIn.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
