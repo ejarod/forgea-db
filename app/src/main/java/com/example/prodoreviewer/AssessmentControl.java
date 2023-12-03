b75eed6e9e8d61a27f6f0e1ca685e4b89fe1f67b
Renz Russell T. Wacan <rrwacan@gmail.com>
2023-12-03 22:25:55 +0800

    Assessment (UI,Result Calculation)

---
+++ app/src/main/java/com/example/prodoreviewer/AssessmentControl.java
@@ -0,0 +1,67 @@
+package com.example.prodoreviewer;
+
+
+public class AssessmentControl {
+    char world;          //E or I
+    char information;    //S or N
+    char decisions;      //T or F
+    char structure;      //J or P
+    String result = assessSkills();
+
+    public String assessSkills() {
+        String result;
+        if(world=='A'){
+            result = "E";
+        } else {
+            result = "I";
+        }
+
+        if(information =='A'){
+            result += "S";
+        } else {
+            result += "N";
+        }
+
+        if(information =='A'){
+            result += "T";
+        } else {
+            result += "F";
+        }
+
+        if(information =='A'){
+            result += "J";
+        } else {
+            result += "P";
+        }
+
+        return result;
+    }
+
+    public AssessmentControl(int[] world, int[] information, int[] decisions, int[]structure) {
+        this.world = getTypeScore(world);
+        this.information = getTypeScore(information);
+        this.decisions = getTypeScore(decisions);
+        this.structure = getTypeScore(structure);
+    }
+
+    public static char getTypeScore(int[] answers) {
+        int scoreA = 0;
+        int scoreB = 0;
+
+        for (int answer : answers) {
+            if (answer < 3) {
+                scoreA += 1;
+            } else {
+                scoreB += 1;
+            }
+        }
+
+        if (scoreA > scoreB) {
+            return 'A';
+        } else if (scoreA < scoreB) {
+            return 'B';
+        } else {
+            return 'C';
+        }
+    }
+}
